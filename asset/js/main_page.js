9c2f29c178f924e696dc2550d412f589d8a75357
Samira OULD JEDDI <samoj@MacBook-Air-de-Samira.local>
2021-06-17 10:54:53 +0200

    html rassemblé

--- asset/js/main_page.js
+++ asset/js/main_page.js
@@ -1,1 +1,28 @@
-document.getElementById('capital').innerHTML = document.getElementById('capital').innerHTML.toUpperCase();
+let logo = document.querySelector('.logo');
+let sub = document.querySelector('#sub');
+let dec = document.querySelector('#dec');
+let actu = document.querySelector('#actu');
+let activ = document.querySelector('#activ');
+let shop = document.querySelector('#shop');
+let forum = document.querySelector('#forum');``
+
+
+dec.addEventListener('click', function (){
+    sub.textContent = 'Découverte';
+});
+actu.addEventListener('click', function (){
+    sub.textContent = 'Actualités';
+});
+activ.addEventListener('click', function (){
+    sub.textContent = 'Activités';
+});
+shop.addEventListener('click', function (){
+    sub.textContent = 'Boutique';
+});
+forum.addEventListener('click', function (){
+    sub.textContent = 'Forum';
+});
+logo.addEventListener('click', function (){
+    sub.textContent = '';
+});
+
